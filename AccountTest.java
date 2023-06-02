52b738db8c6d883ae9f380f35e8a918c354e8583
prince1527 <princeverm631@gmail.com>
2023-06-03 00:04:13 +0530

    AccountTest

---
+++ AccountTest.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class AccountTest {
+	
+	public static void main(String[] args) {
+		Account account1 = new Account("jane Green" , 50.00);
+		Account account2 = new Account("john Blue", -7.53);
+		
+		System.out.printf("%s balance: $%.2f%n",account1.getName(),account1.getBalance());
+		System.out.printf("%s balance: $%.2f%n%n",account2.getName(),account2.getBalance());
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter deposit amount for account1:");
+		double depositAmount = input.nextDouble();
+		System.out.printf("%nadding %.2f to account1 balance%n%n,depositAmount");
+		account1.deposit(depositAmount);
+		
+		System.out.printf("%s balance: $%.2f%n",account1.getName(),account1.getBalance());
+		System.out.printf("%s balance: $%.2f%n%n",account2.getName(),account2.getBalance());
+	}
+
+}
