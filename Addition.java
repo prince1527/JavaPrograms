69bab2a910e7694e3e3ab528cbb1b79728f7244a
prince1527 <princeverm631@gmail.com>
2023-06-04 00:05:44 +0530

    Addition

---
+++ Addition.java
@@ -0,0 +1,15 @@
+import java.io.InputStreamReader;
+import java.util.*;
+public class Addition {
+	public static void main(String args[]) {
+		Scanner num1 = new Scanner(System.in);
+		int a = num1.nextInt();
+		Scanner num2 = new Scanner(System.in);
+		int b = num2.nextInt();
+		System.out.println(a+b);
+		
+		InputStreamReader is = new InputStreamReader(System.in);
+		
+	}
+
+}
